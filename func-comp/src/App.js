223e9ae4bb87ea24388bb19305495ca2e5454e22
deveassy <xjfmwld@gmail.com>
2020-03-28 11:14:46 +0900

    수정파일 업로드

--- func-comp/src/App.js
+++ func-comp/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
@@ -12,10 +12,30 @@ function App() {
 }
 
 function FuncComp(props) {
+  var numberState = useState(props.initNumber); // props로 값을 가져오게 되면 첫번쨰 인자가 2가 됨
+  var number = numberState[0];
+  var setNumber = numberState[1];
+
+//   var dateState = useState((new Date()).toString());
+//   var _date = dateState[0];
+//   var setDate = dateState[1];
+  var [_date, setDate] = useState((new Date()).toString());
+  
   return(
     <div className="App">
       <h2>Funtion Style Component</h2>
-      <p>Number : {props.initNumber}</p>
+      <p>Number : {number}</p>
+      <p>Date : {_date}</p>
+      <input type="button" value="random" onClick={
+          function() {
+            setNumber(Math.random());
+          }
+        }></input>
+        <input type="button" value="date" onClick={
+          function() {
+            setDate((new Date()).toString());
+          }
+        }></input>
     </div>
   );
 }
@@ -23,15 +43,22 @@ function FuncComp(props) {
 class ClassComp extends React.Component {
   state = {
     number: this.props.initNumber,
+    date: (new Date()).toString()
   }
   render() {
     return(
       <div className="App">
         <h2>Class Style Component</h2>
         <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
         <input type="button" value="random" onClick={
           function() {
             this.setState({number:Math.random()})
+          }.bind(this)
+        }></input>
+        <input type="button" value="date" onClick={
+          function() {
+            this.setState({date: (new Date()).toString()})
           }.bind(this)
         }></input>
       </div>
